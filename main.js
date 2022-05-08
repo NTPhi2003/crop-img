3a12296e89c653057b5392e7a1b6b247c78287aa
NTPhi2003 <daorong096@gmail.com>
2022-05-09 00:10:43 +0700

    upload

--- main.js
+++ main.js
@@ -2,28 +2,33 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 const inpFile = $('.inpFile')
 const previewContainer = $('#imagePreview')
-const previewImage = $('.image-preview__image')
-const previewDefaultText = document.querySelector('.image-preview__default-text')
+const previewImage = document.getElementById('fileIMG')
+const previewDefaultText = $('.image-preview__default-text')
 
 inpFile.addEventListener("change", function() {
-    const file = this.files[0]
+    var file = this.files[0]
     
     if (file) {
-        const reader = new FileReader()
+        // const reader = new FileReader()
 
+        
         previewDefaultText.style.display = "none";
         previewImage.style.display = "block";
-        
-        reader.addEventListener("load", function() {
-            console.log(this)
-            previewImage.setAttribute("scr", this.result)
-        })
+        // reader.addEventListener("load", function() {
+        //     console.log(this)
+        //     // previewImage.setAttribute("scr", this.result);
+        //     previewImage.scr = 'this.result'
+            
+        // })
 
-        reader.readAsDataURL(file)
+        // reader.readAsDataURL(file)
+        window.URL = window.URL || window.webkitURL;
+        var blobURL = window.URL.createObjectURL(file);
+        document.getElementById('fileIMG').innerHTML = '<img src="' + blobURL + '" alt="Image preview" class="image-preview__image" />';
 
     } else {
         previewDefaultText.style.display = null;
-        previewImage.style.display = null;
+        previewImage.style.display = "none";
         previewImage.setAttribute("scr", "")
     }
 })
